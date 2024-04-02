72c1f914dc13bdbcb4517e31a6d3c0c13bcf4b92
StayFar <naee0290@mylaurier.ca>
2024-04-02 02:04:19 -0400

    update all custom elements

--- wp-content/themes/assignment-3/customElements.js
+++ wp-content/themes/assignment-3/customElements.js
@@ -21,3 +21,38 @@ class TwoSidedMarket extends HTMLElement{
 }
 
 customElements.define("x-twosided", TwoSidedMarket);
+
+class Products extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="products">Products</products>&nbsp`;
+	}
+}
+
+customElements.define("x-products", Products)
+
+class Email extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="mailto:farizahnaeem07@gmail.com" class="gmail-link">farizahnaeem07@gmail.com</a>`;
+	}
+}
+
+customElements.define("x-email", Email)
+
+
+class PhoneNumber extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="tel:6477600155" class="phone-link">6477600155</a>`;
+	}
+}
+
+customElements.define("x-phone", PhoneNumber)
+
+class Instagram extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="https://www.instagram.com/theflowergirl.07/" class="instagram-link">theflowergirl.07</a>`;
+	}
+}
+
+customElements.define("x-instagram", Instagram)
+
+
