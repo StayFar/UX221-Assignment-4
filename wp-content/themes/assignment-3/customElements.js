9a8cfaa4b7e21b7f3bf1134e92b8e688bb222591
StayFar <naee0290@mylaurier.ca>
2024-04-02 00:46:07 -0400

    update all 5 posts

--- wp-content/themes/assignment-3/customElements.js
+++ wp-content/themes/assignment-3/customElements.js
@@ -6,3 +6,18 @@ class CopyrightYear extends HTMLElement{
 
 customElements.define("x-year", CopyrightYear);
 
+class FarizahWasHere extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = "Farizah was here";
+	}
+}
+
+customElements.define("x-farizah", FarizahWasHere)
+
+class TwoSidedMarket extends HTMLElement{
+	connectedCallback(){
+		this.innerHTML = `<a href="side-a">Side A</a>&nbsp;<a href="side-b">Side B</a>`;
+	}
+}
+
+customElements.define("x-twosided", TwoSidedMarket);
